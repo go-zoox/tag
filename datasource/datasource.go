cec52984fd0f267d7383137341db47712181591b
whwewant <tobewhatwewant@outlook.com>
2022-04-26 09:39:56 +0800

    chore:: update comments

--- datasource/datasource.go
+++ datasource/datasource.go
@@ -2,5 +2,11 @@ package datasource
 
 // DataSource defines the interface for loading data from a data source.
 type DataSource interface {
+	// Get returns the value of the given key.
+	// key support dot notation.
+	// Example:
+	//  - Get("port")
+	//  - Get("redis.port")
+	//  - Get("address.city.houses.0.id")
 	Get(key string) any
 }
--- datasource/map.go
+++ datasource/map.go
@@ -2,7 +2,7 @@ package datasource
 
 import "github.com/go-zoox/core-utils/object"
 
-// MapDataSource is a data source that loads data from a map.
+// MapDataSource is a tag data source that loads data from a map.
 type MapDataSource struct {
 	data map[string]any
 }
@@ -14,6 +14,10 @@ func NewMapDataSource(data map[string]any) *MapDataSource {
 
 // Get returns the value of the given key.
 // key support dot notation.
+// Example:
+//  - Get("port")
+//  - Get("redis.port")
+//  - Get("address.city.houses.0.id")
 func (m *MapDataSource) Get(key string) any {
 	return object.Get(m.data, key)
 }
